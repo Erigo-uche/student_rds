75f214706185e3df67e82499e604fbdad6c803be
Erigo <ucheerigo@gmail.com>
2026-05-14 12:13:41 +0100

    initial commit; completed env, init, config

---
+++ app/init.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+from .config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config) 
+
+    #Ensure logs file exists
+    os.makedirs("logs", exist_ok=True)
+
+    #Logging setup
+    file_handler = RotatingFileHandler(
+        app.config["LOG_FILE"], maxBytes= 10240, backupCount=5
+        )
+    
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s'
+    )
+    file_handler.setFormatter(formatter)
+
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+
+    app.logger.info("App started")
+
+    return app
